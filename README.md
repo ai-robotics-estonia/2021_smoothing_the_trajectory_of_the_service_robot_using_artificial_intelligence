47bffea2a18b794a8435772aa333ec421b458efe
KingBoomie <kristjan.laht13@gmail.com>
2022-08-24 20:01:40 +0300

    First upload

--- README.md
+++ README.md
@@ -1,53 +1,61 @@
-*This is a template repository for this organization. Start by replacing the placeholder for the project name with it's actual title.*
 
-# [Project Name]
+# Smoothing the trajectory of the service robot using artificial intelligence
 
 ## Summary
 *This section is to be filled out by the project manager based on the [summary document](https://docs.google.com/spreadsheets/d/12xi2yOMm-X5PEecgyRe3WEurcSaN9A5z4DHgBacQT6M).*
 
-| Company | [Company](https://website.link) |
-| :--- | :--- |
-| Project Manager | [Project Manager](https://profile.link) |
-| Project Team | [Team Member 1](https://profile.link); [Team Member 2](https://profile.link); ... |
-| Challenge Tackled |  |
-| Technology Used |  |
-| Lessons Learned |  |
-| Result Published |  |
-| Target Group |  |
-| Diagrams/Photos |  |
-| Video |  |
+| Company | [Yanu](https://yanu.ai/)                                                                                                       |
+| :--- |:-------------------------------------------------------------------------------------------------------------------------------|
+| Project Manager | [Project Manager](https://profile.link)                                                                                        |
+| Project Team | Alvo Aabloo, Houman Masnavi, Karl Kruusamäe, Igor Rybalskii, Kirsi Zirel, Kristjan Laht                                        |
+| Challenge Tackled | Developing and testing robot hand trajectory optimization for ROS for smooth robot hand movement                               |
+| Technology Used | ROS, MoveIt, TOPP-RA                                                                                                           |
+| Lessons Learned | Using and debugging general purpose planners is very difficult and requires a lot of specilized knowledge that is hard to get. |
+| Result Published | We integrated optimal trajectory optimization with the ROS robot hand ecosystem                                                |
+| Target Group | Manufacturing floors, robot food providers                                                                                     |
+| Diagrams/Photos |                                                                                                                                |
+| Video |                                                                                                                                |
 
 ## Implementation Details
 
-*This section is to be filled out by the development team. You should describe details about the technical implementation right here. To get started, you can use [this guide](https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/) from freeCodeCamp for inspiration. Please note that some of the sections are already included below but you can add more.*
-
 ### Description
 
-- *What your application does,*
-- *Why you used the technologies you used,*
-- *Some of the challenges you faced and features you hope to implement in the future.*
+- This is a ROS package that processes MoveIt trajectories into a trajectory that is optimal with respect to speed / acceleration / time limits. This is usually a nice to have, but for some robots like the Panda Robot, it's a necessary component for smooth operation. Panda robots require jerk-free trajectories but the using the default MoveIt trajectories fails with a lot of jerk. This is _very_ problematic for service robots that need to carry things. 
+
+- This is built upon ROS and [TOPP-RA](https://github.com/hungpham2511/toppra)
 
 ### How to Install & Run
 
-*Provide a step-by-step description of how to get the development environment set and running.*
+0. [Create a working ROS workspace](http://wiki.ros.org/catkin/Tutorials/create_a_workspace)
+1. [Install toppra](https://github.com/hungpham2511/toppra/blob/develop/cpp/README.md)
+    - Or create a toppra wrapper package by cloning toppra to `catkin_ws/src/toppra-ros/toppra`
+    - and adding a CMake file with [extra dependencies turned off](https://gist.github.com/KingBoomie/348d0379229965d9ab4be9f6d7bb9ce5) to `catkin_ws/src/toppra-ros`
+     
+2. Clone this package into `catkin_ws/src` 
+    ```bash
+    git clone https://github.com/ai-robotics-estonia/smoothing_the_trajectory_of_the_service_robot_using_artificial_intelligence
+    ```
+3. [Setup MoveIt](https://ros-planning.github.io/moveit_tutorials/doc/getting_started/getting_started.html)
+4. in folder `catkin_ws` source ROS workspace `source /opt/ros/noetic && source devel/setup.bash`
+5. build with `catkin_make`
+6. run the ros service `rosrun toppra_optimizer_ros toppra_optimizer_server`
+7. generate a plan using moveit
+8. in another terminal run `rostopic pub /optimize_trajectory/goal toppra_optimizer_ros/OptimizeActionGoal` with the message contents being the result from step 7. (or use [this](https://gist.github.com/KingBoomie/982d5c7e5bec87fda6f558af3451c47e) example generated plan)
+9. run the resulting trajectory on a real robot
 
-### How to Use
-
-*Provide instructions and examples so users/contributors can use the project.*
 
 ### Credits
 
-*If you followed tutorials or referenced a certain material that might help the user to build this particular project, include links to those here.*
+If you use this library for your research, we encourage you to
+reference the accompanying paper [A new approach to Time-Optimal Path Parameterization based on Reachability Analysis, IEEE Transactions on Robotics, vol. 34(3), pp. 645-659, 2018.](https://www.researchgate.net/publication/318671280_A_New_Approach_to_Time-Optimal_Path_Parameterization_Based_on_Reachability_Analysis)
 
 ### Licensing
-
-*Point out your software license. In case this is something different than an MIT license, explain why.*
 
 [MIT License](/LICENSE)
 
 ### How to Contribute
 
-*Add guidelines to let other developers know how they can contribute to your project.*
+Create issues / pull requests in this repo. 
 
 ### Testing
 
---
+++ src/toppra_optimizer_server.cpp
@@ -0,0 +1,301 @@
+#include "ros/ros.h"
+
+#include <toppra/algorithm/toppra.hpp>
+#include <toppra/constraint/linear_joint_acceleration.hpp>
+#include <toppra/constraint/linear_joint_velocity.hpp>
+#include <toppra/geometric_path.hpp>
+#include <toppra/geometric_path/piecewise_poly_path.hpp>
+#include <toppra/parametrizer/const_accel.hpp>
+#include <toppra/toppra.hpp>
+
+#include <toppra/solver/qpOASES-wrapper.hpp>
+
+#include <toppra/solver/glpk-wrapper.hpp>
+
+#include <trajectory_msgs/JointTrajectory.h>
+#include <trajectory_msgs/JointTrajectoryPoint.h>
+
+#include <toppra_optimizer_ros/OptimizeAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+#include <std_msgs/Header.h>
+
+#include <numeric>
+#include <vector>
+#include <string>
+
+void transpose(std::vector<std::vector<double> > &b)
+{
+  if (b.size() == 0)
+    return;
+
+  std::vector<std::vector<double> > trans_vec(b[0].size(), std::vector<double>());
+
+  for (int i = 0; i < b.size(); i++)
+  {
+    for (int j = 0; j < b[i].size(); j++)
+    {
+      trans_vec[j].push_back(b[i][j]);
+    }
+  }
+
+  b = trans_vec;    // <--- reassign here
+}
+
+void formatVecToMat(const std::vector<Eigen::VectorXd, Eigen::aligned_allocator<Eigen::VectorXd>> &vec, Eigen::MatrixXd &mat)
+{
+  mat.resize(vec.at(0).rows(), vec.size());
+  for (size_t i = 0; i < vec.size(); i++)
+  {
+    mat.col(i) = vec.at(i);
+  }
+}
+
+
+double length(const std::vector<double> &q0, const std::vector<double> &q1)
+{
+  auto dof = q0.size();
+  double retval = 0;
+
+  for (size_t i = 0; i < dof; i++) {
+    retval += (q0[i] - q1[i]) * (q0[i] - q1[i]);
+  }
+  return sqrt(retval) / dof;
+}
+
+inline toppra::Vectors makeVectors(
+    const std::vector<std::vector<toppra::value_type>> &v)
+{
+  toppra::Vectors ret;
+  for (auto vi : v) {
+    toppra::Vector vi_eigen(vi.size());
+    for (std::size_t i = 0; i < vi.size(); i++) {
+      vi_eigen(i) = vi[i];
+    }
+    ret.push_back(vi_eigen);
+  }
+  return ret;
+}
+
+
+inline toppra::BoundaryCond makeBoundaryCond(
+    int order, const std::vector<toppra::value_type> &values)
+{
+  toppra::BoundaryCond cond;
+  cond.order = order;
+  cond.values.resize(values.size());
+  for (std::size_t i = 0; i < values.size(); i++) {
+    cond.values(i) = values[i];
+  }
+  return cond;
+}
+
+toppra_optimizer_ros::OptimizeFeedback feedback_;
+toppra_optimizer_ros::OptimizeResult result_;
+
+typedef actionlib::SimpleActionServer<toppra_optimizer_ros::OptimizeAction> Server;
+
+void execute(const toppra_optimizer_ros::OptimizeGoalConstPtr& goal, Server* as)
+{
+
+  trajectory_msgs::JointTrajectory trajectory_to_optimize = goal -> trajectory;
+  std::cout << trajectory_to_optimize << std::endl;
+  
+  int plan_size = trajectory_to_optimize.points.size();
+
+  std::vector<std::vector<double>> joint_positions;
+  for (int i = 0; i < plan_size; i++)
+  {
+    joint_positions.push_back(trajectory_to_optimize.points[i].positions);
+  }
+
+  // create piecewise polynomial geometric path (taken from the file in the issue. line 419)
+  toppra::Vector times;
+  int dof = trajectory_to_optimize.joint_names.size();
+  int numPoints = joint_positions.size();
+  times.resize(numPoints);
+  times(0) = 0.;
+  for (int i = 1; i < numPoints; i++)
+  {
+    times(i) = times(i - 1) + length(joint_positions[i - 1], joint_positions[i]);
+  }
+
+  //boundary condition. 2 means we specify acceleration, after that starting/ending acceleration is specified
+  const std::vector<double> bound_cond_zeros(0, dof); //boundary condition is defined with the number of joints in mind
+  toppra::BoundaryCond bc = makeBoundaryCond(2, bound_cond_zeros);
+  std::array<toppra::BoundaryCond, 2> bc_type{ bc, bc };
+
+  auto toppra_joint_positions = makeVectors(joint_positions);
+
+  //combine into path
+  auto path = std::make_shared<toppra::PiecewisePolyPath>(toppra_joint_positions, times, bc_type);
+
+  std::vector<int> points(joint_positions.size());
+  std::iota (std::begin(points), std::end(points), 0); 
+
+  //set vel and acc limit and put them as constrains
+
+  //TODO read limits from param server according to joint names from trajectory
+
+  std::vector<std::string> joint_names;
+  std::vector<double> joint_velocity_limits;
+  std::vector<double> joint_accelaration_limits;
+
+  for (auto &&joint_name : trajectory_to_optimize.joint_names)
+  {
+    joint_names.push_back(joint_name);
+  }
+
+  for (auto &&joint_name : joint_names)
+  {
+    double vel_limit, acc_limit;
+    if (ros::param::get("/robot_description_planning/joint_limits/"+joint_name+"/max_velocity", vel_limit))
+    {
+      joint_velocity_limits.push_back(vel_limit);
+    }
+    else
+    {
+      
+      ROS_ERROR_STREAM("Could not find ros parameter for joint velocity limit for joint " << joint_name);
+      //should through exception
+    }
+
+    if (ros::param::get("/robot_description_planning/joint_limits/"+joint_name+"/max_acceleration", acc_limit))
+    {
+      joint_accelaration_limits.push_back(acc_limit);
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Could not find ros parameter joint acceleration limit for joint " << joint_name);
+      //should through exception
+    }
+
+  }
+
+  // const std::vector<double> joint_velocity_limits_c = joint_velocity_limits;
+  // const std::vector<double> joint_accelaration_limits_c = joint_accelaration_limits;
+
+  //WIP: convert std vector to toppra(Eigen) vector. currently build fails
+  // auto velLimitLower = -toppra::Vector(joint_velocity_limits.data());
+  // auto velLimitUpper = toppra::Vector(joint_velocity_limits.data());
+
+  // auto accLimitLower = -toppra::Vector(joint_accelaration_limits.data());
+  // auto accLimitUpper = toppra::Vector(joint_accelaration_limits.data());
+
+  double velMin = *std::min_element(joint_velocity_limits.begin(), joint_velocity_limits.end());  //current workaround for toppra vector issue
+  double accMin = *std::min_element(joint_accelaration_limits.begin(), joint_accelaration_limits.end());
+
+  auto velLimitLower = -1.0 * velMin * toppra::Vector::Ones(dof);
+  auto velLimitUpper = velMin * toppra::Vector::Ones(dof);
+  auto accLimitLower = -1.0 * accMin * toppra::Vector::Ones(dof);
+  auto accLimitUpper = accMin * toppra::Vector::Ones(dof);
+
+  // auto velLimitLowerT = toppra::Vector::
+
+  toppra::LinearConstraintPtr ljv, lja;
+  ljv = std::make_shared<toppra::constraint::LinearJointVelocity>
+    (velLimitLower, velLimitUpper);
+  lja = std::make_shared<toppra::constraint::LinearJointAcceleration>
+    (accLimitLower, accLimitUpper);
+
+
+  toppra::LinearConstraintPtrs constrains =
+    toppra::LinearConstraintPtrs{ljv, lja};
+
+  //make an algorithm with path and constrains
+  toppra::PathParametrizationAlgorithmPtr algo =
+    std::make_shared<toppra::algorithm::TOPPRA>(constrains, path);
+
+  //compute the algorithm and get the resulted data
+  toppra::ReturnCode rc1 = algo->computePathParametrization(0, 0);
+  toppra::ParametrizationData pd = algo->getParameterizationData();
+
+  toppra::Vector gridpoints =
+      pd.gridpoints; // Grid-points used for solving the discretized problem.
+  toppra::Vector vsquared =
+      pd.parametrization; // Output parametrization (squared path velocity)
+
+  std::shared_ptr<toppra::parametrizer::ConstAccel> ca =
+      std::make_shared<toppra::parametrizer::ConstAccel>(path, gridpoints,
+                                                          vsquared);
+
+  //time(?) length of the path
+  Eigen::Matrix<toppra::value_type, 1, 2> interval2;
+  interval2 = ca->pathInterval();
+
+  if (true)
+      std::cout << "ca->validate() = " << ca->validate() << std::endl;
+
+  //how many thetas you want
+  const int length2 = plan_size; //same amount as was in the original plan
+  toppra::Vector times2 =
+      toppra::Vector::LinSpaced(length2, interval2(0), interval2(1)); //make timestamps to get corresponding thetas
+
+    std::cout << "interval2 = " << interval2 << std::endl;
+
+  //take thetas, acc and vel for computed timestamps
+  toppra::Vectors path_pos2;
+  path_pos2 = ca->eval(times2, 0);
+  toppra::Vectors path_vel2;
+  path_vel2 = ca->eval(times2, 1);
+  toppra::Vectors path_acc2;
+  path_acc2 = ca->eval(times2, 2);
+
+  //converting the data to matrix format
+  Eigen::MatrixXd path_pos2_ = Eigen::MatrixXd::Zero(dof, length2);
+  Eigen::MatrixXd path_vel2_ = Eigen::MatrixXd::Zero(dof, length2);
+  Eigen::MatrixXd path_acc2_ = Eigen::MatrixXd::Zero(dof, length2);
+  formatVecToMat(path_pos2, path_pos2_);
+  formatVecToMat(path_vel2, path_vel2_);
+  formatVecToMat(path_acc2, path_acc2_);
+
+  //rotate the matrix to be the shape of [plan_size][dof]
+  Eigen::MatrixXd path_pos2_proper = Eigen::MatrixXd::Zero(length2, dof);
+  Eigen::MatrixXd path_vel2_proper = Eigen::MatrixXd::Zero(length2, dof);
+  Eigen::MatrixXd path_acc2_proper = Eigen::MatrixXd::Zero(length2, dof);
+  path_pos2_proper = path_pos2_.reverse().transpose().reverse();
+  path_vel2_proper = path_vel2_.reverse().transpose().reverse();
+  path_acc2_proper = path_acc2_.reverse().transpose().reverse();
+  times2 = times2.transpose();
+
+  //create plan and start to fill it with computed values
+  trajectory_msgs::JointTrajectory final_plan;
+
+  for (int i = 0; i < length2; i++)
+  {
+    trajectory_msgs::JointTrajectoryPoint plan_increment;
+    for (int j = 0; j < dof; j++)
+    {
+      plan_increment.accelerations.push_back(path_acc2_proper(i,j));
+      plan_increment.positions.push_back(path_pos2_proper(i,j));
+      plan_increment.velocities.push_back(path_vel2_proper(i,j));
+    }
+    plan_increment.time_from_start = ros::Duration(times2(i));
+    final_plan.points.push_back(plan_increment);
+  }
+
+  final_plan.joint_names = trajectory_to_optimize.joint_names;
+  final_plan.header = trajectory_to_optimize.header;
+
+  result_.optimized_trajectory = final_plan;
+  feedback_.optimization_success = true;
+
+  as->publishFeedback(feedback_);
+  as->setSucceeded(result_);
+  
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "toppra_optimizer_action_client");
+  ros::NodeHandle _nh;
+
+  Server server(_nh, "optimize_trajectory", boost::bind(&execute, _1, &server), false);
+  
+  server.start();
+  ros::spin();
+
+  return 0;
+}
+
